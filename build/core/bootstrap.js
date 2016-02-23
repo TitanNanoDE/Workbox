8711dd3eb77fe9b1649d9dd9de36cbbb107c81fc
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2016-02-23 19:11:31 +0100

    remove the build dir from git
    The build dir shouldn't be checked in, it's just a place to asseble everything during the build process.

--- build/core/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import System from './System.js';
-import TerminalPackage from '../packages/Terminal/main.js';
-
-let TerminalApplication = System.PackageLoader.bootstrapPackage(TerminalPackage);
-System.ApplicationManager.register(TerminalApplication);
-System.ApplicationManager.launch('System::Terminal', System);
